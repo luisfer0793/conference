4c1696dc0b40777ffaa7a9e27e4eea174950bc06
Fernando <luisfer.0793@gmail.com>
2019-09-08 02:03:36 -0500

    Diseño principal creado, HTML & CSS funcional.

---
+++ index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>IT Conferences</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="menu layout-container">
+            <ul class="menu__link-box">
+                <li class="menu__item"><a href="#" class="menu__link">Conferencias</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Calendario</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Invitados</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Reservaciones</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Contacto</a></li>
+            </ul>
+            <ul class="menu__link-box">
+                <li class="menu__item"><a href="#" class="menu__link">Iniciar Sesión</a></li>
+                <li class="menu__item"><a href="#" class="menu__link">Registrarse</a></li>
+            </ul>
+        </nav>
+        <div class="header__title-box">
+            <h2 class="header__title">I T <span class="header__title--bold">Conference</span></h2>
+            <h2 class="header__subtitle">El mejor sitio de conferencias de desarrollo de software</h2>
+            <a href="#" class="button">Conoce Mas</a>
+        </div>
+    </header>
+    <nav class="navbar">
+        <div class="navbar-box layout-container">
+            <p class="navbar__title">Conference</p>
+            <ul class="navbar__link-box">
+                <li class="navbar__item"><a href="#" class="navbar__link">Conferencia</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Calendario</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Invitados</a></li>
+            </ul>
+        </div>
+    </nav>
+    <main class="main layout-main">
+        <div class="layout-container--medium">
+            <h1 class="main__title title">El mejor sitio web de conferencias de tecnología</h1>
+            <div class="main__description-grid">
+                <div class="main__description-box">
+                    <h2 class="main__description-title">Ponentes</h2>
+                    <p class="main__description">
+                        En Conference podrás encontrar variada cantidad de expositores con amplia experiencia y conocimientos para compartir.
+                    </p>
+                </div>
+                <div class="main__description-box">
+                    <h2 class="main__description-title">Escenarios</h2>
+                    <p class="main__description">
+                        Sabemos que parte de un buen aprendizaje también es influenciado en la comodidad de donde se estudia, en Conference no hay de que preocuparse, siempre encontraremos el mejor lugar para la comodidad de nuestros visitantes.
+                    </p>
+                </div>
+                <div class="main__description-box">
+                    <h2 class="main__description-title">Temas</h2>
+                    <p class="main__description">
+                        Cientos de temas por difundir son parte de nuestro de nuestro pefil; siempre que aprender, siempre algo nuevo por descubrir.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+    <section class="section__video">
+        <div class="section__video-box">
+            <video class="video" autoplay loop muted>
+                <source src="./video/video-bg.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="section__video-program">
+            <h2 class="title">Programa</h2>
+            <div class="section__video-grid">
+                <div class="section__video-info">
+                    <a href="#" class="section__video-info-link"><i class="section__video-icon icon ion-ios-code"></i>Talleres</a>
+                    <a href="#" class="section__video-info-link"><i class="section__video-icon icon ion-ios-chatbubbles"></i>Conferencias</a>
+                    <a href="#" class="section__video-info-link"><i class="section__video-icon icon ion-ios-people"></i>Seminarios</a>
+                </div>
+                <div class="section__video-info">
+                    <h4>Front End Development</h4>
+                    <p class="section__video-info-description">Conoce las técnicas mas utilizadas para dominar el desarrollo Front End.</p>
+                    <p class="section__video-info-description--small"><i class="section__video-icon icon ion-ios-clock"></i> 15:00</p>
+                    <p class="section__video-info-description--small"><i class="section__video-icon icon ion-ios-calendar"></i> 10 septiembre</p>
+                    <p class="section__video-info-description--small"><i class="section__video-icon icon ion-ios-person"></i> Luis Fernando Jiménez</p>
+                </div>
+                <div class="section__video-info">
+                    <h4>Back End Development</h4>
+                    <p class="section__video-info-description">Conoce las técnicas mas utilizadas para dominar el desarrollo Back End.</p>
+                    <p class="section__video-info-description--small"><i class="section__video-icon icon ion-ios-clock"></i> 15:00</p>
+                    <p class="section__video-info-description--small"><i class="section__video-icon icon ion-ios-calendar"></i> 10 septiembre</p>
+                    <p class="section__video-info-description--small"><i class="section__video-icon icon ion-ios-person"></i> Luis Fernando Jiménez</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section layout-section">
+        <div class="layout-container--medium">
+            <h2 class="title">Próxima Conferencia</h2>
+            <div class="timer__grid">
+                <div class="timer__number-box">
+                    <span class="timer__number">10</span>
+                    <p class="timer__number-description">Días</p>
+                </div>
+                <div class="timer__number-box">
+                    <span class="timer__number">15</span>
+                    <p class="timer__number-description">Horas</p>
+                </div>
+                <div class="timer__number-box">
+                    <span class="timer__number">30</span>
+                    <p class="timer__number-description">Minutos</p>
+                </div>
+                <div class="timer__number-box">
+                    <span class="timer__number">25</span>
+                    <p class="timer__number-description">Segundos</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="newsletter section layout-section">
+        <div class="layout-container">
+            <p class="newsletter__title">Suscribete al Newsletter Mensual</p>
+            <h2 class="newsletter__title--bold">CONFERENCE</h2>
+            <a href="#" class="button">Registarse</a>
+        </div>
+    </section>
+</body>
+</html>
